2fcc3e026c0a3c84b510aa0d0814f3c88afa06ef
Thomas DUMOND <thomas.dumond@etu.unistra.fr>
2022-02-02 20:58:06 +0100

    tp2

---
+++ TP2/ordonnanceur.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+
+#define CHK(op)                                                                \
+    do {                                                                       \
+        if ((op) == -1)                                                        \
+            raler(1, #op);                                                     \
+    } while (0)
+
+noreturn void raler(int syserr, const char *msg, ...) {
+    va_list ap;
+
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    fprintf(stderr, "\n");
+    va_end(ap);
+
+    if (syserr == 1)
+        perror("");
+
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+    uintmax_t duree_qtum = (uintmax_t)atoi(argv[1]);
+}
