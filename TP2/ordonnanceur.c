19e6cbb529754b8e4fc93728c03f4612bb6a3ff5
Thomas DUMOND <thomas.dumond@etu.unistra.fr>
2022-02-08 15:12:46 +0100

    TP2

--- TP2/ordonnanceur.c
+++ TP2/ordonnanceur.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -26,7 +27,11 @@ noreturn void raler(int syserr, const char *msg, ...) {
     exit(EXIT_FAILURE);
 }
 
-int stop = 0;
+volatile sig_atomic_t signal_usr2 = 0;
+
+volatile sig_atomic_t signal_child = 0;
+
+volatile sig_atomic_t process_to_kill = 0;
 
 void election(intmax_t process_id) {
     printf("SURP - process %ju\n", process_id);
@@ -40,7 +45,34 @@ void eviction(intmax_t process_id) {
     printf("EVIP - process %ju\n", process_id);
 }
 
-void process(int temps) { (void)temps; }
+void process_fils(sigset_t *masque_usr1) {
+    CHK(sigsuspend(masque_usr1));
+    while (!signal_usr2) {
+        sleep(1);
+    }
+    signal_usr2 = 0;
+    exit(0);
+}
+
+void signal_handler(int signum) {
+    switch (signum) {
+    case SIGUSR1:
+
+        break;
+
+    case SIGUSR2:
+        signal_usr2 = 1;
+        break;
+
+    case SIGCHLD:
+        signal_child = 1;
+        break;
+
+    case SIGALRM:
+        process_to_kill = 1;
+        break;
+    }
+}
 
 int main(int argc, char **argv) {
 
@@ -52,7 +84,7 @@ int main(int argc, char **argv) {
     }
 
     intmax_t nb_process = (intmax_t)argc - 2;
-    intmax_t duree_qtum = (intmax_t)atoi(argv[1]);
+    int duree_qtum = atoi(argv[1]);
 
     // Tests durée quantums et process
     if (duree_qtum < 1) {
@@ -67,9 +99,30 @@ int main(int argc, char **argv) {
     // Tableau des pid de tous les processus fils
     pid_t *process_id = malloc(sizeof(pid_t) * nb_process);
 
+    // Tableau des codes de retour des processus fils
+    int *terminaison = calloc(1, sizeof(int) * nb_process);
+
     // Masques pere et fils (par héritage)
+    sigset_t masque_fils, masque_pere;
+    CHK(sigemptyset(&masque_fils));
+    CHK(sigaddset(&masque_fils, SIGUSR1));
+    CHK(sigemptyset(&masque_pere));
+    CHK(sigaddset(&masque_pere, SIGCHLD));
+    CHK(sigaddset(&masque_pere, SIGALRM));
+
+    // Redirection des signaux
+    struct sigaction usr1, usr2, chld, alarm;
+    usr1.sa_handler = signal_handler;
+    usr2.sa_handler = signal_handler;
+    chld.sa_handler = signal_handler;
+    alarm.sa_handler = signal_handler;
+    CHK(sigaction(SIGUSR1, &usr1, NULL));
+    CHK(sigaction(SIGUSR2, &usr2, NULL));
+    CHK(sigaction(SIGCHLD, &chld, NULL));
+    CHK(sigaction(SIGALRM, &alarm, NULL));
 
     for (intmax_t k = 0; k < nb_process; k++) {
+        // Liste de tous les pid des process fils
         process_id[k] = fork();
 
         switch (process_id[k]) {
@@ -77,18 +130,39 @@ int main(int argc, char **argv) {
             raler(1, "fork");
 
         case 0:
-            pause();
-            process(atoi(argv[k + 2]));
-            exit(0);
+            process_fils(&masque_fils);
         }
     }
 
     // Tant que tous les fils ne sont pas finis
     while (fin == 0) {
         for (intmax_t k = 0; k < nb_process; k++) {
-            // Mettre prgm k
+            // Si processus k fini alors on le passe
+            if (terminaison[k] == 0) {
+                goto process_suivant;
+            }
+
+            // Démarrage de l'alarme sur la duré d'un quantum
+            alarm(duree_qtum);
+
+            // Lancement du processus k
+            kill(process_id[k], SIGUSR1);
+
             // Attente SIGALARM OU SIGCHLD
+            CHK(sigsuspend(&masque_pere));
+
             // Si SIGCHLD -> process k a ne plus considérer
+            if (signal_child == 1) {
+                signal_child = 0;
+                int raison;
+                CHK(wait(&raison));
+                terminaison[k] = 0;
+
+            } else { // Sinon si on a reçu l'alarme
+                kill(process_id[k], SIGUSR2);
+            }
+
+        process_suivant:;
         }
     }
 
