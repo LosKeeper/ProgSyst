563c66360536fd1d6c618caddbe48403cee87204
Thomas DUMOND <thomas.dumond@etu.unistra.fr>
2022-02-09 23:25:56 +0100

    TP2

--- TP2/ordonnanceur.c
+++ TP2/ordonnanceur.c
@@ -34,7 +34,7 @@ volatile sig_atomic_t signal_usr2 = 0;
 
 volatile sig_atomic_t signal_child = 0;
 
-volatile sig_atomic_t process_to_kill = 0;
+volatile sig_atomic_t signal_alarme = 0;
 
 void election(intmax_t process_id) {
     printf("SURP - process %ju\n", process_id);
@@ -51,19 +51,27 @@ void eviction(intmax_t process_id) {
     fflush(stdout);
 }
 
-void process_fils(sigset_t *masque_usr1, int *nb_quantums, pid_t pid_pere) {
+void process_fils(sigset_t *masque_usr, sigset_t *vide, int *nb_quantums,
+                  pid_t pid_pere) {
+    (void)masque_usr;
     while (*nb_quantums != 0) {
+        // Démasquer tous les signaux SIGUSR1 ET 2
+        // !CHK(sigprocmask(SIG_SETMASK, vide, NULL));
+
         while (!signal_usr1) {
-            sigsuspend(masque_usr1);
+            sigsuspend(vide);
         }
         signal_usr1 = 0;
+
         while (!signal_usr2) {
-            // Masquer tous les signaux
             sleep(1);
-            // Démasquer tout les signaux
         }
-        (*nb_quantums)--;
         signal_usr2 = 0;
+
+        // Masquer tous les signaux envoyés par le pére
+        // !CHK(sigprocmask(SIG_BLOCK, masque_usr, vide));
+
+        (*nb_quantums)--;
         if (*nb_quantums == 0) {
             CHK(kill(pid_pere, SIGCHLD));
             exit(0);
@@ -88,7 +96,7 @@ void signal_handler(int signum) {
         break;
 
     case SIGALRM:
-        process_to_kill = 1;
+        signal_alarme = 1;
         break;
     }
 }
@@ -114,9 +122,6 @@ int main(int argc, char **argv) {
     // Tableau des pid de tous les processus fils
     pid_t *process_id = malloc(sizeof(pid_t) * nb_process);
 
-    // Tableau de terminaison des processus fils
-    int *terminaison = calloc(0, sizeof(int) * nb_process);
-
     // Tableau du nombre de quantums par fils
     int *nb_quantums = malloc(sizeof(int) * nb_process);
     for (int i = 0; i < nb_process; i++) {
@@ -131,13 +136,16 @@ int main(int argc, char **argv) {
     }
 
     // Masques pere et fils (par héritage)
-    sigset_t masque_fils, masque_pere;
-    CHK(sigemptyset(&masque_fils));
-    // CHK(sigaddset(&masque_fils, SIGUSR1));
-    // CHK(sigaddset(&masque_fils, SIGUSR2));
-    CHK(sigemptyset(&masque_pere));
-    // CHK(sigaddset(&masque_pere, SIGCHLD));
-    // CHK(sigaddset(&masque_pere, SIGALRM));
+    sigset_t vide, masque_signaux_fils, masque_alarme, masque_fils;
+    CHK(sigemptyset(&vide));
+    CHK(sigemptyset(&masque_signaux_fils));
+    CHK(sigaddset(&masque_signaux_fils, SIGCHLD));
+    CHK(sigaddset(&masque_signaux_fils, SIGUSR1));
+    CHK(sigemptyset(&masque_alarme));
+    CHK(sigaddset(&masque_alarme, SIGALRM));
+    CHK(sigemptyset(&masque_signaux_fils));
+    CHK(sigaddset(&masque_fils, SIGUSR1));
+    CHK(sigaddset(&masque_fils, SIGUSR2));
 
     // Redirection des signaux
     struct sigaction usr1, usr2, chld, alarme;
@@ -159,17 +167,24 @@ int main(int argc, char **argv) {
             raler(1, "fork");
 
         case 0:
-            process_fils(&masque_fils, &nb_quantums[k], pid_pere);
+            // Sous-prgm processus fils
+            process_fils(&masque_fils, &vide, &nb_quantums[k], pid_pere);
         }
     }
 
     // Tant que tous les fils ne sont pas finis
     while (nb_process_fini < nb_process) {
         for (intmax_t k = 0; k < nb_process; k++) {
+            // Masquage des signaux envoyés par le fils
+            CHK(sigprocmask(SIG_BLOCK, &masque_signaux_fils, &vide));
+
             // Si processus k fini alors on le passe
-            if (terminaison[k] == 1) {
+            if (process_id[k] == -1) {
                 goto process_suivant;
             }
+
+            // Démasquer alarme
+            CHK(sigprocmask(SIG_SETMASK, &vide, NULL));
 
             // Démarrage de l'alarme sur la duré d'un quantum
             alarm(duree_qtum);
@@ -179,25 +194,34 @@ int main(int argc, char **argv) {
             election(k);
 
             // Attente SIGALARM
-            // Démasquer alarme
-            sigsuspend(&masque_pere);
+            while (!signal_alarme) {
+                sigsuspend(&vide);
+            }
+            signal_alarme = 0;
+
             // Masquer alarme
+            CHK(sigprocmask(SIG_BLOCK, &masque_alarme, &vide));
+
+            // Démasquage des signaux envoyés par le fils
+            CHK(sigprocmask(SIG_SETMASK, &vide, NULL));
 
             // Envoie commande de terminaison au processus
             CHK(kill(process_id[k], SIGUSR2));
             eviction(k);
 
             // Attente SIGCHLD ou SIGUSR1
-            sigsuspend(&masque_pere);
+            sigsuspend(&vide);
+
+            // Masquage des signaux envoyés par le fils
+            CHK(sigprocmask(SIG_BLOCK, &masque_signaux_fils, NULL));
 
             // Si SIGCHLD -> process k a ne plus considérer
             if (signal_child == 1) {
                 signal_child = 0;
                 nb_process_fini++;
-                terminaison[k] = 1;
+                process_id[k] = -1;
                 int raison;
                 CHK(wait(&raison));
-                terminaison[k] = 1;
                 terminaison_print(k);
 
             } else if (signal_usr1 == 1) { // Si SIGUSR1 alors process non fini
@@ -207,6 +231,7 @@ int main(int argc, char **argv) {
         process_suivant:;
         }
     }
-
+    free(process_id);
+    free(nb_quantums);
     return 0;
 }
